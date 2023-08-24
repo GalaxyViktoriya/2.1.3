badb89a024d0a85ca52aec4dbaeb467a7f1246d6
Ruslan Garmanov <garmanov.rus@mail.ru>
2023-08-24 06:36:31 +0300

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(cat1.getText());
+        System.out.println(bean == bean1);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,8 @@
+public class Cat {
+    private String text;
+
+    public String getText() { return text; }
+
+    public void setText(String text) { this.text = text; }
+
+}
